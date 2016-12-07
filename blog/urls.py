ecf486026918567fd16849bd7ca4ededd9d1e5d6
tsotnesharvadze <tsotnesharvadze@gmail.com>
2016-12-08 00:37:27 +0400

    damamate template falebi

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from .views import *
 urlpatterns=[
 
-url(r'', index ,name="index"),
-
+url(r'^$', index ,name="index"),
+url(r'^(?P<question_id>[0-9]+)/$',detail,name="detail"),
+url(r'^(?P<question_id>[0-9]+)/results/$',results,name="results"),
+url(r'^(?P<question_id>[0-9]+)/vote/$',vote,name="vote"),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
+
+
 def index(request):
-	return HttpResponse("hello")
+	latest_question_list = Question.objects.order_by('-pub_date')[:6]
+	context={'latest_question_list':latest_question_list}
+	return render(request,'blog/index.html',context)
+
+def detail(request,question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("კითხვა არ არსებობს")
+	return render(request,'blog/detail.html',{'question':question})
+
+def results(request,question_id):
+	response="შენ უყურებ რეზულტატს შეკითხვისა %s"
+	return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+	return HttpResponse("შენ ხმას აძლევ კითხვას %s"%question_id)
